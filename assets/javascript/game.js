69a7b5cccce3bb07d67c966401f1e028d16542e3
Jalia Evans <irohdesign@outlook.com>
2018-12-06 13:31:31 -0500

    writing pseudocode and setting up file tree

---
+++ assets/javascript/game.js
@@ -0,0 +1,32 @@
+
+//pseudocode
+
+//create an array for all of the letters to be stored in
+
+
+//start this function onkeyup
+//computer randomly selects a letter
+
+
+
+//user enters their letter
+
+
+//compare the user letter to the computer's letter
+
+    //userGuess === computerGuess => victory
+
+        //make alert to tell them they've won
+
+        //count how many times they've won
+
+    //userGuess !== computerGuess => incorrect (make a shake animation?)
+        //make alert to tell them it's wrong
+
+        //count how many guesses left
+
+        //alert when there's no guesses left
+
+        //count how many times they went to 0
+
+//restart the game WITHOUT REFRESH, keeping the losses counter
